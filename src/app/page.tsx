16a63ae62990e40b195188c0b3c552c96d7709a5
Daniel Santamaria <santamaria.daniel316@gmail.com>
2024-12-20 01:54:27 -0800

    Add user authentication via Resend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,32 @@
 "use server";
 
+import { auth } from "@/auth";
 import { generateImage } from "./actions/generateImage";
 import ImageGenerator from "./components/ImageGenerator";
 
 export default async function Home() {
-  return <ImageGenerator generateImage={generateImage} />;
+  const session = await auth();
+  return (
+    <>
+      {!session ? (
+        <>
+          <h1 className="mb-4 text-2xl">Welcome to the Pentagram</h1>
+          <p className="mb-4">Please sign in to access.</p>
+          <a
+            href="/api/auth/signin"
+            className="inline-block rounded border p-2"
+          >
+            Sign In
+          </a>
+        </>
+      ) : (
+        <>
+          <ImageGenerator generateImage={generateImage} />
+          {/* <a href="/api/auth/signout" className="mt-4 inline-block rounded border p-2">
+              Sign Out
+            </a> */}
+        </>
+      )}
+    </>
+  );
 }
