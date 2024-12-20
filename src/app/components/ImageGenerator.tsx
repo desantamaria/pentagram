9d54cf9aaee334da3a627a8488952fb5fa1c75e0
Daniel Santamaria <santamaria.daniel316@gmail.com>
2024-12-20 15:51:07 -0800

    Tweak Layout of page

--- src/app/components/ImageGenerator.tsx
+++ src/app/components/ImageGenerator.tsx
@@ -88,42 +88,69 @@ export default function ImageGenerator({
   };
 
   return (
-    <div className="min-h-screen flex flex-col justify-between p-8">
+    <div className="h-screen w-screen bg-gradient-to-l from-gray-200 via-fuchsia-200 to-stone-100 flex flex-col justify-between p-8">
       <main className="flex-1">
         {/* Main content can go here */}
-        <Button asChild>
-          <Link href="/api/auth/signout">Sign Out</Link>
-        </Button>
 
-        <h1>Welcome {session.user?.email}</h1>
+        <div className="flex justify-end items-center gap-2 mb-10">
+          <h1>
+            Welcome{" "}
+            <span className="text-purple-500">{session.user?.email}</span>{" "}
+          </h1>
+          <Button asChild>
+            <Link
+              href="/api/auth/signout"
+              className="text-white relative bg-gradient-to-r from-purple-500 via-pink-500 to-purple-500 
+              bg-[size:200%_auto] hover:bg-[position:100%_0] motion-safe:transition-[background-position] 
+              motion-safe:duration-500 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2"
+            >
+              Sign Out
+            </Link>
+          </Button>
+        </div>
 
-        {error && (
-          <div className="w-full max-w-2xl bg-red-50 border border-red-200 rounded-lg">
-            {error}
-          </div>
-        )}
+        <div className="flex flex-col items-center justify-center gap-3 overflow-scroll">
+          {images.length > 0 && (
+            <div className="w-1/2 max-w-2xl rounded-lg overflow-hidden shadow-lg">
+              {images.map(image => (
+                <img
+                  key={image.id}
+                  src={image.url}
+                  alt={`Generated artwork ${image.url}`}
+                  className="w-full h-auto"
+                ></img>
+              ))}
+            </div>
+          )}
 
-        {images.length > 0 && (
-          <div className="w-full max-w-2xl rounded-lg overflow-hidden shadow-lg">
-            {images.map(image => (
-              <img
-                key={image.id}
-                src={image.url}
-                alt={`Generated artwork ${image.url}`}
-                className="w-1/3 h-auto"
-              ></img>
-            ))}
-          </div>
-        )}
+          {isLoading && (
+            <div>
+              <svg
+                aria-hidden="true"
+                className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-purple-500"
+                viewBox="0 0 100 101"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                  fill="currentColor"
+                />
+                <path
+                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                  fill="currentFill"
+                />
+              </svg>
+              <span className="sr-only">Loading...</span>
+            </div>
+          )}
 
-        {isLoading && (
-          <div className="w-full max-w-2xl flex items-center justify-center">
-            <div
-              className="w-full h-12 border-4 border-gray-300 border-t-black dark:border-gray-50"
-              style={{ animation: "spin 1s linear infinite" }}
-            />
-          </div>
-        )}
+          {error && (
+            <div className="w-full max-w-2xl bg-red-50 border border-red-200 rounded-lg">
+              {error}
+            </div>
+          )}
+        </div>
       </main>
 
       <footer className="w-full max-w-3xl mx-auto">
